588e40503e7e47b6be404a9492dcf456a818b6de
sheldonjpowell <sheldonjpowell@yahoo.com>
2022-07-15 15:34:38 -0500

    First Commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd6583581f8msha521fe9d71bb2c0p1bef6djsn0fee20cafaca',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
